dd6fe32a4a9ea7e537ef371d473c45cfaecf3054
Aaron Levine <levine.aa@husky.neu.edu>
2017-04-17 22:54:33 -0400

    added ffmpeg and conversion

--- autoupload.py
+++ autoupload.py
@@ -1,23 +1,31 @@
 from __future__ import print_function
 import sys
 import time
+import ffmpy
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 
 
 class HandleDVR(PatternMatchingEventHandler):
-    patterns = ["*.txt"]
+    patterns = ["*.mp4"]
 
     @staticmethod
     def process(event):
-        print(event.src_path, event.event_type)
+        print(event.src_path)
+        convert(event.src_path)
 
     def on_modified(self, event):
-        self.process(event)
+        pass
 
     def on_created(self, event):
         self.process(event)
 
+def convert(input_path):
+    conversion = ffmpy.FFmpeg(
+            inputs={input_path: None},
+            outputs={input_path.replace('.mp4', '.gif'): None}
+    )
+    conversion.run()
 
 def main():
     path = sys.argv[1] if len(sys.argv) > 1 else '.'
